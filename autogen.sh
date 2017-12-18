f9561893dac1d1030f1091c29106cb5050b2f371
Jason Gerecke <killertofu@gmail.com>
2017-12-18 11:43:58 -0800

    Use git-version-gen to set AC_INIT version data
    The AC_INIT macro is used to define basic package information such as the
    version number. At the moment, we have to manually bump this number as
    part of the release process. The version number information is already
    available from git, however, meaning that we can instead use a script like
    git-version-gen to fill it in.
    
    Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,11 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
+VERSION=`./git-version-gen -u`
+if ! grep -qs "^#define PACKAGE_VERSION \"${VERSION}\"\$" config.h; then
+	touch configure.ac
+fi
+
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
